10c53574def10852739a11ba7ec2aeff80189503
Esteban Pautt <estebandaniel.pauttmontt@unitecnar.edu.co>
2025-09-03 12:16:39 -0500

    Tarea form java

---
+++ Formulario Java/src/formulario/java/FormularioJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formulario.java;
+
+import Formularios.FormNotas;
+
+/**
+ *
+ * @author ESTUDIANTES
+ */
+public class FormularioJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FormNotas().setVisible(true);
+    }
+    
+}
