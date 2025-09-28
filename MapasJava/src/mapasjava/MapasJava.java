f26159a0c290955468dc96b4d81e10010b29f6fc
Esteban Pautt <estebandaniel.pauttmontt@unitecnar.edu.co>
2025-09-28 16:58:31 -0500

    Add files via upload

--- MapasJava/src/mapasjava/MapasJava.java
+++ MapasJava/src/mapasjava/MapasJava.java
@@ -4,6 +4,8 @@
  */
 package mapasjava;
 
+import Formularios.FrmEstudiantes;
+
 /**
  *
  * @author esteb
@@ -14,7 +16,8 @@ public class MapasJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> new FrmEstudiantes().setVisible(true));
+
     }
     
 }
